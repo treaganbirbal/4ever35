b5b2b8f0c9640e480538660c92d0995018773725
Treagan Birbal <treaganbirbal@pursuit.org>
2019-11-19 23:02:34 -0500

    display img, title, nav

--- src/App.js
+++ src/App.js
@@ -2,17 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { removeTypeDuplicates } from '@babel/types';
+import axios from 'axios'
+
+import Nav from './components/Nav'
 
 class App extends React.Component {
   constructor(){
     super()
       this.state = {
-
+        products: []
       }
+  }
+
+  componentDidMount(){
+    axios.get("http://localhost:3004/sweaters")
+      .then(res => {
+        this.setState({
+          products: res.data
+        })
+      })
   }
   render(){
     return (
-      <>TEster</>
+      <>
+      <Nav />
+      {this.state.products.map(product => {
+        return (
+          <>
+          <p>{product.title}</p>
+          <img src = {product.img}/>
+          </>
+        )
+      })}
+      
+      </>
     )
   }
    
---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import {router} from 'react-router-dom'
+
+const Nav = () => {
+    return (
+        <li>  Nav  </li>
+    )
+}
+
+export default Nav
